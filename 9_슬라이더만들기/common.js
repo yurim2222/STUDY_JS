2bb3ab8765de1864baf76907aac9fd0ebe183e03
yurim <yurim2222@gmail.com>
2021-03-29 14:18:59 +0900

    js파일이 길어져서 따로 빼고, next버튼 누를 시 무한루프되도록 코드 수정함

---
+++ 9_슬라이더만들기/common.js
@@ -0,0 +1,47 @@
+            const slideList = document.querySelector('.slide_list');
+            const slideContents = document.querySelectorAll('.slide_content');
+            const slideBtnNext = document.querySelector('.slide_btn_next');
+            const slideBtnPrev = document.querySelector('.slide_btn_prev');
+            const slideWidth = 400;
+            const slideSpeed = 300;
+            const slideLength = slideContents.length;
+            const startNum = 0;
+
+
+            let curIndex = startNum;
+            let curSlide = slideContents[curIndex];
+            curSlide.classList.add('slide_active');
+
+            slideList.style.width = slideWidth * (slideLength + 2) + 'px';
+
+            //copy first and last slide
+            let firstChild = slideList.firstElementChild;
+            let lastChild = slideList.lastElementChild;
+            let clonedFirst = firstChild.cloneNode(true);
+            let clonedLast = lastChild.cloneNode(true);
+
+            //add copied Slide
+            slideList.appendChild(clonedFirst);
+            slideList.insertBefore(clonedLast, slideList.firstElementChild);
+
+            slideList.style.transform = `translateX(-${slideWidth * (startNum + 1)}px)`;
+
+
+            //btn click event
+            //next
+            slideBtnNext.addEventListener('click',function(){
+                if(curIndex <= slideLength - 1){
+                    slideList.style.transform = `translateX(-${slideWidth * (curIndex + 2)}px)`;                    
+                    slideList.style.transition = slideSpeed + 'ms';
+                }
+                if(curIndex === slideLength - 1){
+                    setTimeout(function(){
+                        slideList.style.transition = '0ms';
+                        slideList.style.transform = `translateX(-${slideWidth}px)`;
+                    },slideSpeed);
+                    curIndex = -1;
+                }
+                curSlide.classList.remove('slide_active');
+                curSlide = slideContents[++curIndex];
+                curSlide.classList.add('slide_active');
+            });
