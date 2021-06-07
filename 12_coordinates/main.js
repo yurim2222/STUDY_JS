0e23cc8bd6740d4c03349c23996118a169d4656c
yurim <yurim2222@gmail.com>
2021-06-07 11:34:54 +0900

    좌표찾기 성능개선

--- 12_coordinates/main.js
+++ 12_coordinates/main.js
@@ -3,17 +3,21 @@ const horozontal = document.querySelector(".horozontal");
 const vertical = document.querySelector(".vertical");
 const target = document.querySelector(".target");
 
-document.addEventListener("mousemove", (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
-    tag.innerHTML = `
-        ${x}px, ${y}px
-    `;
-    vertical.style.left = `${x}px`;
-    horozontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
+addEventListener('load', () => {
     
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
+
+    document.addEventListener("mousemove", (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+        tag.innerHTML = `
+            ${x}px, ${y}px
+        `;
+        vertical.style.transform = `translate(${x}px, 0)`;
+        horozontal.style.transform = `translate(0, ${y}px)`;
+        target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
+        tag.style.transform = `translate(${x+30}px, ${y+30}px)`;
+    });
 });
