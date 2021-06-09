592429e100b824c8193988f00fd752646848ec2f
yurim <yurim2222@gmail.com>
2021-06-09 22:45:29 +0900

    MINIGAME: before refacktoring

---
+++ 15_mini_game/correct.js
@@ -0,0 +1,199 @@
+'use strict'
+
+const field = document.querySelector(".game_field");
+const fieldRect = field.getBoundingClientRect();
+const popUp = document.querySelector(".pop-up");
+const popUpMSG = document.querySelector(".pop-up__message");
+const gameReplayBtn = document.querySelector(".pop-up__refresh");
+
+const CARROT_COUNT = 2;
+const BUG_COUND = 4;
+
+const gamePlayBtn = document.querySelector(".game__button");
+const gameTimer = document.querySelector(".game__timer");
+const gameScore = document.querySelector(".game__score");
+
+const bgSound = new Audio('./sound/bg.mp3');
+const carrotSound = new Audio('./sound/carrot_pull.mp3');
+const bugSound = new Audio('./sound/bug_pull.mp3');
+const winSound = new Audio('./sound/game_win.mp3');
+const alertSound = new Audio('./sound/game_win.mp3');
+
+let item = undefined;
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+
+gamePlayBtn.addEventListener("click", () => {
+    if(started){
+        stopGame();
+    }else{
+        startGame();
+    }
+});
+
+
+field.addEventListener("click", e => onFieldClick(e));
+
+gameReplayBtn.addEventListener("click", (e) => {
+    startGame();
+    hidePopUp();
+});
+
+const startGame = () => {
+    started = true;
+    initGame();
+    showStopBtn();
+    showTimerAndScore();
+    startGameTimer();
+    playSound(bgSound);
+};
+
+const stopGame = () => {
+    started = false;
+    stopGameTimer();
+    hideGameBtn();
+    showPopUpWithText('Replay?🤪');
+};
+
+const finishGame = (win) => {
+    started = false;
+    hideGameBtn();
+    console.log(win);
+    showPopUpWithText( win ? 'YOU WON 😆' : 'YOU LOSE😥');
+    started ? playSound(bgSound) : pauseSound(bgSound);
+    !win && playSound(alertSound);
+    score = 0;
+};
+
+const showStopBtn = () => {
+    gamePlayBtn.style.visibility = "initial";
+    const icon = gamePlayBtn.querySelector(".fas");
+    icon.classList.add("fa-stop");
+    icon.classList.remove("fa-play");
+};
+
+const hideGameBtn = () => {
+    gamePlayBtn.style.visibility = "hidden";
+    gameTimer.style.visibility = "hidden";
+    gameScore.style.visibility = "hidden";
+};
+
+const showTimerAndScore = () => {
+    gameTimer.style.visibility = "visible";
+    gameScore.style.visibility = "visible";
+};
+
+const showPopUpWithText = (text) => {
+    popUp.style.display = "block";
+    popUpMSG.innerText = text;
+};
+
+const hidePopUp = () => {
+    popUp.style.display = "none";
+}
+
+const onFieldClick = (e) => {
+    if(!started){
+        return;
+    }
+    if(e.target.className === 'carrot'){
+        playSound(carrotSound);
+        e.target.remove();
+        score++;
+        updateScoreBoard();
+        if( score === CARROT_COUNT ){
+            finishGame(true);
+            stopGameTimer();
+        };
+    }else if(e.target.className === 'bug'){
+        playSound(bugSound);
+        stopGameTimer();
+        finishGame(false);
+    };
+};
+
+const updateScoreBoard = () => {
+    gameScore.innerText = CARROT_COUNT - score;
+};
+
+let GAME_DURATION_SEC = 10;
+const startGameTimer = () => {
+    let remainingTimer = GAME_DURATION_SEC;
+    updateTimerText(remainingTimer);
+    timer = setInterval(() => {
+        updateTimerText(--remainingTimer);
+        console.log(remainingTimer);
+        console.log(GAME_DURATION_SEC);
+        if(remainingTimer <= 0){
+            stopGameTimer();
+            finishGame(false);
+            return;
+        };
+    },1000);
+};
+
+let stopGameTimer = () => {
+    clearInterval(timer);
+    return;
+};
+
+const updateTimerText = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    gameTimer.innerText = `${minutes}:${seconds}`
+};
+
+
+const initGame = () => {
+    field.innerHTML = '';
+    gameScore.innerText = CARROT_COUNT;
+    addItem('carrot', CARROT_COUNT , './img/carrot.png');
+    addItem('bug', BUG_COUND , './img/bug.png');
+};
+
+
+
+const addItem = (className, count, imgPath) => {
+    const x1 = 0;
+    const y1 = 0;
+    const imgSize = "80";
+    const x2 = fieldRect.width - imgSize;
+    const y2 = fieldRect.height - imgSize;
+
+    for(let i = 0 ; i < count ; i++){
+        item = document.createElement('img');
+        item.setAttribute("class", className);
+        item.setAttribute("src", imgPath);
+        const x = randomNumber(x1, x2);
+        const y = randomNumber(y1, y2);
+        item.style.top = `${y}px`;
+        item.style.left = `${x}px`;
+        field.appendChild(item);
+    };
+};
+
+
+const randomNumber = (min, max) => {
+    return Math.random() * (max - min) + min;
+};
+
+const playSound = (sound) => {
+    sound.currentTime = 0;
+    sound.play();
+};
+
+const pauseSound = (sound) => {
+    sound.pause();
+};
+
+
+
+
+
+
+
+
+
